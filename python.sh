ad9df443245027ae0a59a463b51873c639a4e98e
kimbo <kimballleavitt@gmail.com>
2020-02-07 11:48:14 -0700

    fix args to virtualenv and default syntax

--- python.sh
+++ python.sh
@@ -2,25 +2,28 @@
 #
 # shell commands to make python version management easier
 #
-DEFAULT_PYTHON_VERSION=python
-DEFAULT_VIRTUALENV_NAME=venv
+default_python_version=python
+default_virtualenv_name=venv
 
 v() {
-	version="${1:-$DEFAULT_PYTHON_VERSION}"
-	path="${2:-$DEFAULT_VIRTUALENV_NAME}"
-	virtualenv --python "$version"
+	version="${1:-$default_python_version}"
+	path="${2:-$default_virtualenv_name}"
+	virtualenv --python "$version" "$path"
 }
 
 v3() {
-	path={$1:$DEFAULT_VIRTUALENV_NAME}
+	path="${1:-$default_virtualenv_name}"
 	v python3 "$path"
 }
 
 v2() {
-	path={$1:$DEFAULT_VIRTUALENV_NAME}
+	path="${1:-$default_virtualenv_name}"
 	v python2 "$path"
 }
 
-alias a="activate"
-alias d="deactivate"
+a() {
+	dir="${1:-$default_virtualenv_name}"
+	. "$dir/bin/activate"
+}
 
+alias d="deactivate"
